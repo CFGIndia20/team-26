e41b61dc0d1905cdd76df2cb6096ba7c655e3507
Harsh Kukreja <harshkukreja99@gmail.com>
2020-07-26 06:21:25 +0530

    question response done

--- st-judes-backend/app/Http/Controllers/QuestionController.php
+++ st-judes-backend/app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Helper\ResponseHelper;
 use App\Question;
+use App\QuestionResponse;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -14,5 +15,23 @@ class QuestionController extends Controller
 
     public function getAllQuestion() {
         return ResponseHelper::success( Question::with('unit')->get());
+    }
+
+    public function postQuestionResponse(Request $request) {
+        $patient = $request->input('patient');
+        $questions = $request->input('question');
+
+        foreach ($questions as $question) {
+            $questionResponse = QuestionResponse::create([
+                "patient_id" => $patient,
+                "question_id" => $question->question_id,
+                "rating" => $question->rating,
+                "description" => $question->description,
+            ]);
+            if ($questionResponse) {
+                return ResponseHelper::created($questionResponse);
+            }
+            return  ResponseHelper::badRequest();
+        }
     }
 }
